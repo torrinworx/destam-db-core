11cbc2b8ae7d56fc5700bc1e48cb0ce7646d87a3
torrin1 <torrin@worx4you.com>
2024-12-21 17:20:52 -0500

    removing comment

--- drivers/server/fs.js
+++ drivers/server/fs.js
@@ -1,6 +1,5 @@
 
 export default () => {
-    // Code needed to start the driver method here, connecting to db,  creating file system, etc.
     return {
         init: () => {},
         update: () => {},
